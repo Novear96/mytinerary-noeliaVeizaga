fb0098716f6ad8a9b33d66acebb68b28e6a1357a
noelia <noeliaveizaga96@gmail.com>
2024-03-08 15:19:28 -0300

    tailwind

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
-
 function App() {
 
   return (
-    <>
-    <div>
-      <h1>Hola mundo</h1>
+    <div className="w-full min-h-screen flex flex-col bg-zinc-800 text-white">
+      <div className="px-3 flex justify-between items-center">
+        <img src="/vite.svg" alt="logo" className="w-14 h-10" />
+        <p>MyTinerary</p>
+        <nav>
+          <a href="#">Home</a>
+          <a href="">Cities</a>
+          <a href="">Contac</a>
+        </nav>
+      </div>
+      <header>
+        <img src="/vite.svg" alt="" />
+      </header>
+      <main className="grow">Main</main>
+      <footer></footer>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
